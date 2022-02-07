3dd687c31fbd19888561ca02839dab93a3322b1b
Clark Schneider <cschneider@verticalrelevance.com>
2022-02-07 17:35:07 +0000

    add IaC from CodeCommit opa-eval-serverless
    Former-commit-id: f04463d959c61c7f44bad25dec46a79e4043cfc6

---
+++ lambda/functions/opa-eval/install_opa.sh
@@ -0,0 +1,2 @@
+curl -L -o opa https://github.com/open-policy-agent/opa/releases/latest/download/opa_linux_amd64
+chmod 755 ./opa
---
+++ modules/repo-bucket-sync/outputs.tf
@@ -0,0 +1,3 @@
+output "codepipeline_arn" {
+    value = aws_codepipeline.repo_bucket_sync.arn
+}
