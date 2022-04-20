1d52091e490b0dcd1399d386cb0f5ca9e3a5b82b
Clark Schneider <cschneider@verticalrelevance.com>
2022-04-20 21:49:23 +0000

    HandleInfraction ddb, eb
    Former-commit-id: 61eb0c7fd3eb7028b51157755572604b75cce485

--- supplementary_files/lambdas/handle-infraction/lambda_function.py
+++ supplementary_files/lambdas/handle-infraction/lambda_function.py
@@ -1,7 +1,107 @@
+import os
+import json
+
+import boto3
+from botocore.exceptions import ClientError
+
+ddb = boto3.resource('dynamodb')
+eb = boto3.client('events')
+
+def update_item(*,
+    Table,
+    Pk,
+    Sk,
+    Attributes:list
+):
+    
+    def ddb_compatible_type(Item):
+        if type(Item) == float:
+            return str(Item)
+        else:
+            return Item
+    
+    table = ddb.Table(Table)
+    
+    expression_attribute_values = {}
+    
+    update_expressions = []
+    
+    for i,v in enumerate(Attributes):
+        key = list(v.keys())[0]
+        print(v[key])
+        
+        placeholder = f':{chr(97+i)}'
+        expression_attribute_values[placeholder] = ddb_compatible_type(v[key])
+        
+        update_expressions.append(f'{key}={placeholder}')
+        
+    update_expression = ', '.join(update_expressions)
+    update_expression = f'set {update_expression}'
+    
+    try:
+        r = table.update_item(
+            
+            Key = {
+                'pk':Pk,
+                'sk':Sk
+            },
+            UpdateExpression = update_expression,
+            ExpressionAttributeValues = expression_attribute_values
+        )
+    except ClientError as e:
+        print(f'ClientError:\n{e}')
+        return False
+    else:
+        print(r)
+        return True
+
+def put_event_entry(*,
+    EventBusName,
+    Detail:dict
+):
+    try:
+        r = eb.put_events(
+            Entries = [
+                {
+                    'EventBusName':EventBusName,
+                    'Detail':json.dumps(Detail)
+                }
+            ]
+        )
+    except ClientError as e:
+        print(f'ClientError:\n{e}')
+        return False
+    else:
+        print(r)
+        return True
+
 def lambda_handler(event, context):
     
     print(event)
     
+    event_bus_name = os.environ.get('EventBusName')
+    
+    infraction_key = list(event['Infraction'].keys())[0]
+    
     # to ddb
     
+    update_item(
+        Table = os.environ.get('TableName'),
+        Pk = event['OuterEvalEngineSfnExecutionId'],
+        Sk = infraction_key,
+        Attributes = [
+            {'Metadata':'someMetadata'},
+            {'OuterEvalEngineSfnExecutionId':event['OuterEvalEngineSfnExecutionId']}
+        ]
+    )
+    
     # to eb
+    
+    put_event_entry(
+        EventBusName = os.environ.get('EventBusName'),
+        Detail = {
+            'Infraction':event['Infraction'],
+            'Metadata':'someMetadata',
+            'OuterEvalEngineSfnExecutionId':event['OuterEvalEngineSfnExecutionId']
+        }
+    )
