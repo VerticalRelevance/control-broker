18ea97893cda8bb3a59d09659929a098a5e104ef
Eddie Peters <epeters@verticalrelevance.com>
2022-04-21 11:06:47 -0400

    Lowercase canary name
    Former-commit-id: b869f1315f5504c05d086a4d11d35a33e7c44c4f

--- stacks/test_stack.py
+++ stacks/test_stack.py
@@ -112,7 +112,7 @@ class TestStack(Stack):
             start_canary_after_creation=True,
             artifact_s3_location=canary_bucket.s3_url_for_object("canary_artifacts/"),
             execution_role_arn=canary_execution_role.role_arn,
-            name="ValidCfnTemplateTest",
+            name="validcfntemplatetest",
             schedule=aws_synthetics.CfnCanary.ScheduleProperty(
                 expression="rate(1 minute)", duration_in_seconds="600"
             ),
