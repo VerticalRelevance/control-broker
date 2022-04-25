b73cf3e9f9bc30ed4cb6c899e9ffc543952bd8ea
Clark Schneider <cschneider@verticalrelevance.com>
2022-04-25 19:10:45 +0000

    apigw lambda debug prints
    Former-commit-id: 37a66948a80513bb59b280085853f88b3285cf9d

--- supplementary_files/lambdas/invoked_by_apigw/lambda_function.py
+++ supplementary_files/lambdas/invoked_by_apigw/lambda_function.py
@@ -6,4 +6,10 @@ from botocore.exceptions import ClientError
 def lambda_handler(event,context):
     print(event)
     
+    headers = event.get('headers')
+    
+    auth_header = headers.get('authortization')
+    
+    print(f'auth_header:\n{auth_header}')
+    
     return True
