627dc813d7598aa4f7846d3d1f0cb582cabd45c3
Clark Schneider <cschneider@verticalrelevance.com>
2022-04-27 18:50:53 +0000

    SignApigwRequest passes full event as control_broker_consumer_input
    Former-commit-id: b68e8aefea2361f60d75dad62830e4646e419504

--- supplementary_files/lambdas/write_result_report/lambda_function.py
+++ supplementary_files/lambdas/write_result_report/lambda_function.py
@@ -6,6 +6,27 @@ from botocore.exceptions import ClientError
 
 ddb = boto3.resource('dynamodb')
 
+def simple_pk_query(*,
+    Table,
+    Pk,
+):
+    try:
+        r = ddb.query(
+            TableName=Table,
+            ExpressionAttributeValues = {
+                ":pk": {
+                    "S": Pk
+                }
+            },
+            KeyConditionExpression="pk = :pk"
+        )
+    except ClientError as e:
+        raise
+    else:
+        items = r['Items']
+        print(items)
+        return items
+
 def lambda_handler(event, context):
     
     print(event)
@@ -13,5 +34,14 @@ def lambda_handler(event, context):
     sfn_exec_id = os.environ.get('OuterEvalEngineSfnExecutionId')
     
     print(f'sfn_exec_id:\n{sfn_exec_id}')
+    
+    eval_results_table = os.environ.get('EvalResultsTable')
+    
+    print(f'eval_results_table:\n{eval_results_table}')
+    
+    items = simple_pk_query(
+        Table = eval_results_table,
+        Pk = eval_results_table
+    )
     
     return True
