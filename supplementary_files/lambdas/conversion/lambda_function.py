0206fc6bcea87b996ecc3dbee6e343f470f2b012
Clark Schneider <cschneider@verticalrelevance.com>
2022-05-02 14:42:43 +0000

    Issue #3 rm ConfigRule, factor out to consumer repo
    Former-commit-id: 592326a04a20e9ea28cdf806e97fd4e3e9425b56

---
+++ supplementary_files/lambdas/conversion/lambda_function.py
@@ -0,0 +1,26 @@
+
+
+
+
+# user-provided top-level key: 'ConfigEvent'
+
+# check input
+
+# is Config
+
+# look i
+
+def convert_config_to_cfn():
+    pass
+
+convert: {
+    "ConfigEvent":convert_config_to_cfn ,
+    # "SAM": convert_sam_to_cfn#TODO
+    # "HelmChart": #TODO
+    # "Terraform": #TODO
+}
+
+
+# policies can trust what CB sends it,
+
+# policies don't have to check input type
