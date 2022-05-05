9183ef1288b076ee3722672eee9fda8908af4f88
Clark Schneider <cschneider@verticalrelevance.com>
2022-05-05 18:07:48 +0000

    start integration

--- stacks/handlers_stack.py
+++ stacks/handlers_stack.py
@@ -31,6 +31,37 @@ class HandlersStack(Stack):
 
     def endpoint(self):
         
+        # integration
+        
+        lambda_invoked_by_apigw = aws_lambda.Function(
+            self,
+            "InvokedByApigw",
+            runtime=aws_lambda.Runtime.PYTHON_3_9,
+            handler="lambda_function.lambda_handler",
+            timeout=Duration.seconds(60),
+            memory_size=1024,
+            code=aws_lambda.Code.from_asset(
+                "./supplementary_files/lambdas/invoked_by_apigw"
+            ),
+            environment={
+                "ControlBrokerOuterSfnArn": self.control_broker_outer_state_machine.state_machine_arn,
+                "ControlBrokerEvalResultsReportsBucket": self.control_broker_eval_results_bucket.bucket_name,
+            },
+        )
+
+        lambda_invoked_by_apigw.role.add_to_policy(
+            aws_iam.PolicyStatement(
+                actions=[
+                    "states:StartExecution",
+                ],
+                resources=[self.control_broker_outer_state_machine.state_machine_arn],
+            )
+        )
+        
+        integration = aws_apigatewayv2_integrations_alpha.HttpLambdaIntegration(
+            "ControlBrokerClient", lambda_invoked_by_apigw
+        )
+        
         # api
 
         self.http_api = aws_apigatewayv2_alpha.HttpApi(
@@ -45,7 +76,7 @@ class HandlersStack(Stack):
         routes = self.http_api.add_routes(
             path=self.paths['CloudFormation'],
             methods=[aws_apigatewayv2_alpha.HttpMethod.POST],
-            # integration=integration,
+            integration=integration,
             # authorizer=authorizer_lambda
             # authorizer=authorizer_iam
         )
