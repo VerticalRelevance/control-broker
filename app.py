0497721e9e7e5078fb3846faed796e902d81e2e8
Clark Schneider <cschneider@verticalrelevance.com>
2022-05-20 17:59:00 +0000

    start pipeline stack

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import os
 import aws_cdk as cdk
 
 from stacks.handlers_stack import HandlersStack
+from stacks.pipeline_stack import GitHubCDKPipelineStack
 
 STACK_VERSION = "V0x10x0"
 
@@ -19,4 +20,16 @@ handlers_stack = HandlersStack(
     env=env,
     pac_framework=app.node.try_get_context("control-broker/pac-framework"),
 )
+
+if app.node.try_get_context("control-broker/continuous-deployment/enabled"):
+    github_config = app.node.try_get_context("control-broker/continuous-deployment/github-config")
+    pipeline_stack = GitHubCDKPipelineStack(
+        app,
+        f"CBPipelineStack{STACK_VERSION}",
+        env=env,
+        github_repo_name = github_config["github_repo_name"],
+        github_repo_owner = github_config["github_repo_owner"],
+        github_repo_branch = github_config["github_repo_branch"],
+    )
+
 app.synth()
