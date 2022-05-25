4d9da1994f4cf8756f18c14a92a2b847aa6a41c2
Clark Schneider <cschneider@verticalrelevance.com>
2022-05-25 18:22:29 +0000

    rename HandlersStack to ControlBrokerStack

--- supplementary_files/lambdas/apigw_authorizer/lambda_function.py
+++ supplementary_files/lambdas/apigw_authorizer/lambda_function.py
@@ -3,7 +3,7 @@ import json
 import boto3
 from botocore.exceptions import ClientError
 
-def determine_is_authorized():
+def determine_is_authorized(auth_header):
     # TODO
     return True
 
@@ -11,10 +11,11 @@ def lambda_handler(event,context):
     print(event)
     
     headers = event.get('headers')
+    print(f'headers:\n{headers}')
     
-    auth_header = headers.get('authorization')
+    auth_header = headers.get('authorization') # undercase here, even if request is uppercase
     
-    is_authorized = determine_is_authorized()
+    is_authorized = determine_is_authorized(auth_header)
     
     auth_decision = {
       "isAuthorized": is_authorized,
