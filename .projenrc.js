7b4cf3148ea7a8650b6e50931a19034bd1e31532
Eddie Peters <eddie@dinogalactic.com>
2022-06-22 16:32:10 -0400

    fix: Peg @types/jest version to 27 to fix depedency conflicts and jsii compilation

--- .projenrc.js
+++ .projenrc.js
@@ -1,17 +1,17 @@
-const { awscdk } = require("projen");
+const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: "Clark Schneider",
-  authorAddress: "cschneider@verticalrelevance.com",
-  cdkVersion: "2.28.1",
-  defaultReleaseBranch: "main",
-  name: "control-broker",
-  repositoryUrl: "git@github.com:verticalrelevance/control-broker",
-  typescriptVersion: "4.7.3",
+  author: 'Clark Schneider',
+  authorAddress: 'cschneider@verticalrelevance.com',
+  cdkVersion: '2.28.1',
+  defaultReleaseBranch: 'main',
+  name: 'control-broker',
+  repositoryUrl: 'git@github.com:verticalrelevance/control-broker',
+  typescriptVersion: '4.7.3',
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
-project.addDevDeps("@types/jest==^27.0.0");
+project.addDevDeps('@types/jest@^27.0.0', 'prettier-eslint');
 project.synth();
