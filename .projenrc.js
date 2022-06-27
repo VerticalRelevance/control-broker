23e6f9a4354e4aa1919fe7121ebc2f5bd684ef7a
Eddie Peters <epeters@verticalrelevance.com>
2022-06-27 15:26:39 -0400

    Try publishing to PyPi
    Former-commit-id: 0c3a5a65c015984c95e3c8993dbff36d86d6f8d5

--- .projenrc.js
+++ .projenrc.js
@@ -1,22 +1,37 @@
-const { awscdk } = require('projen');
+const { awscdk } = require("projen");
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: 'Clark Schneider',
-  authorAddress: 'cschneider@verticalrelevance.com',
-  cdkVersion: '2.28.1',
-  defaultReleaseBranch: 'main',
+  author: "Clark Schneider",
+  authorAddress: "cschneider@verticalrelevance.com",
+  cdkVersion: "2.28.1",
+  defaultReleaseBranch: "main",
   releaseBranches: { release: { majorVersion: 0 } },
-  name: 'control-broker',
-  repositoryUrl: 'git@github.com:verticalrelevance/control-broker',
-  typescriptVersion: '4.7.3',
-  deps: ['@aws-cdk/aws-apigatewayv2-alpha@2.28.1-alpha.0'],
+  name: "control-broker",
+  repositoryUrl: "git@github.com:verticalrelevance/control-broker",
+  typescriptVersion: "4.7.3",
+  deps: ["@aws-cdk/aws-apigatewayv2-alpha@2.28.1-alpha.0"],
   description:
-    'Control Broker allows customers to deploy an HTTP API on AWS that executes Policy as Code (PaC) policies using Open Policy Agent (OPA) or CloudFormation Guard to evaluate inputs and return decisions.',
-  packageName: 'control-broker',
-  peerDeps: ['@aws-cdk/aws-apigatewayv2-alpha@2.28.1-alpha.0'],
+    "Control Broker allows customers to deploy an HTTP API on AWS that executes Policy as Code (PaC) policies using Open Policy Agent (OPA) or CloudFormation Guard to evaluate inputs and return decisions.",
+  packageName: "control-broker",
+  peerDeps: ["@aws-cdk/aws-apigatewayv2-alpha@2.28.1-alpha.0"],
+  publishToPypi: {
+    distName: "control-broker",
+    module: "control_broker",
+  },
 });
-project.addDevDeps('@types/jest@^27.0.0', 'prettier-eslint');
+
+project.addDevDeps("@types/jest@^27.0.0", "prettier-eslint");
 // Add environment specification that causes the release workflow to run in the context of the "npm" GitHub environment,
 // which contains the required NPM_TOKEN
-project.tryFindObjectFile('.github/workflows/release-release.yml').addOverride('jobs.release_npm.environment', 'npm');
-project.tryFindObjectFile('.github/workflows/release.yml').addOverride('jobs.release_npm.environment', 'npm');
+project
+  .tryFindObjectFile(".github/workflows/release-release.yml")
+  .addOverride("jobs.release_npm.environment", "npm");
+project
+  .tryFindObjectFile(".github/workflows/release.yml")
+  .addOverride("jobs.release_npm.environment", "npm");
+project
+  .tryFindObjectFile(".github/workflows/release-release.yml")
+  .addOverride("jobs.release_pypi.environment", "pypi");
+project
+  .tryFindObjectFile(".github/workflows/release.yml")
+  .addOverride("jobs.release_pypi.environment", "pypi");
 project.synth();
