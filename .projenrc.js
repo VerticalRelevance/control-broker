5382a0950513f3a759a742f4fa252ac10327c354
Eddie Peters <epeters@verticalrelevance.com>
2022-06-27 14:49:18 -0400

    Reduce description length.
    Former-commit-id: e7d6767de489476a0a5793a1feaca3ee0c83f23f

--- .projenrc.js
+++ .projenrc.js
@@ -10,7 +10,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   typescriptVersion: '4.7.3',
   deps: ['@aws-cdk/aws-apigatewayv2-alpha@2.28.1-alpha.0'],
   description:
-    'Control Broker allows customers to deploy an HTTP API on AWS that executes Policy as Code (PaC) policies using Open Policy Agent (OPA) or CloudFormation Guard to evaluate inputs and return results. Control Broker is an event-driven, serverless AWS-based API created by Vertical Relevance (VR). Control Broker is a VR Module, which is a ready-to-deploy infrastructure solution that implements a portion of VR’s Control Foundations Solution. Control Broker comes with examples and defaults that can help organizations get started in using PaC on AWS.',
+    'Control Broker allows customers to deploy an HTTP API on AWS that executes Policy as Code (PaC) policies using Open Policy Agent (OPA) or CloudFormation Guard to evaluate inputs and return decisions.',
   packageName: 'control-broker',
   peerDeps: ['@aws-cdk/aws-apigatewayv2-alpha@2.28.1-alpha.0'],
 });
