ea69037b1a374146c865403c1ef452a987faa0dd
Eddie Peters <epeters@verticalrelevance.com>
2022-06-30 09:37:35 -0400

    Mock Lambda function in tests to avoid using Docker during unit tests.

--- test/control-broker.test.ts
+++ test/control-broker.test.ts
@@ -1,7 +1,20 @@
+import { PythonFunction } from '@aws-cdk/aws-lambda-python-alpha';
 import { Stack } from 'aws-cdk-lib';
 import { Api, ControlBroker, OpaEvalEngine } from '../src';
 import { HttpApiBinding } from '../src/constructs/api-bindings';
 import { CloudFormationInputHandler } from '../src/constructs/input-handlers';
+
+jest.mock('@aws-cdk/aws-lambda-python-alpha');
+
+const mockedPythonFunction = <jest.Mock<typeof PythonFunction>>(PythonFunction as unknown);
+mockedPythonFunction.mockImplementation(() => {
+  const original = jest.requireActual('@aws-cdk/aws-lambda-python-alpha');
+  return {
+    ...original.PythonFunction,
+    functionArn: 'arn:aws:lambda:us-east-1:123456789012:function:mockfunction',
+    addPermission: () => {},
+  };
+});
 
 test('ControlBroker can be created and attached to a stack', () => {
   const stack = new Stack();
@@ -10,6 +23,7 @@ test('ControlBroker can be created and attached to a stack', () => {
   const cfnInputHandlerApiBinding = new HttpApiBinding('CloudFormation', api, cfnInputHandler);
   const evalEngine = new OpaEvalEngine(stack, 'EvalEngine');
   const evalEngineBinding = new HttpApiBinding('EvalEngine', api, evalEngine);
+  expect(mockedPythonFunction).toHaveBeenCalled();
   api.setEvalEngine(evalEngine, evalEngineBinding);
   api.addInputHandler(cfnInputHandler, cfnInputHandlerApiBinding);
   new ControlBroker(stack, 'TestControlBroker', {
