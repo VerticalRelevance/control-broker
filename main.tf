b54ed49902c4f305e8812419d3c58abce5fa924b
Clark Schneider <cschneider@verticalrelevance.com>
2022-08-27 13:12:47 +0000

    sagemaker RK demo resources

--- main.tf
+++ main.tf
@@ -38,3 +38,18 @@ locals {
 data "aws_caller_identity" "i" {}
 
 data "aws_organizations_organization" "o" {}
+
+##################################################################
+#               sagemaker RK demo resources
+##################################################################
+
+locals{
+  console_sm_role="arn:aws:iam::446960196218:role/service-role/AmazonSageMaker-ExecutionRole-20220827T090729"
+}
+
+resource "aws_sagemaker_notebook_instance" "i" {
+  name          = "${local.resource_prefix}"
+  role_arn      = local.console_sm_role
+  instance_type = "ml.t2.medium"
+
+}
